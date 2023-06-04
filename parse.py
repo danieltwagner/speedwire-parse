3eeaa0f94ad0a8fb6861d3988290136fb417cdcc
Daniel Wagner <dtw@dropbox.com>
2023-06-04 20:39:17 +0100

    dict for parsing

--- parse.py
+++ parse.py
@@ -12,6 +12,8 @@ TAG_END = b'\x00\x00'
 TAG_SMA_NET_2 = b'\x00\x10'
 GROUP1 = b'\x00\x00\x00\x01'
 
+class UnkownProtocolException(Exception):
+    pass
 
 def parse_sma_packet(ip):
     udp = ip.data
@@ -19,7 +21,7 @@ def parse_sma_packet(ip):
 
     # ensure the packet has the SMA header
     if udp.data[:4] != b"SMA\x00":
-        return None, None
+        raise UnkownProtocolException("First 4 UDP package bytes are " + str(udp.data[:4]))
     
     # parse tags
     read = 4
@@ -48,70 +50,98 @@ def parse_sma_packet(ip):
 def parse_sma_net_packet(data):
     protocol_id = data[:2]
     if protocol_id != b'\x60\x81':
-        return None
+        raise UnkownProtocolException("SMA Protocol ID is " + str(protocol_id))
 
-    parsed = ""
     # The changing bytes
-    parsed += str(int.from_bytes(data[10:14])) + ","
-    parsed += str(int.from_bytes(data[20:22])) + ","
-    parsed += str(int.from_bytes(data[31:34])) + ","
-    parsed += str(int.from_bytes(data[39:42])) + ","
-    parsed += str(int.from_bytes(data[50:54])) + ","
-    parsed += str(int.from_bytes(data[60:62])) + ","
-    parsed += str(int.from_bytes(data[70:74])) + ","
-    parsed += str(int.from_bytes(data[80:82])) + ","
-    parsed += str(int.from_bytes(data[90:94])) + ","
-    parsed += str(int.from_bytes(data[100:102])) + ","
-    parsed += str(int.from_bytes(data[111:114])) + ","
-    parsed += str(int.from_bytes(data[119:122])) + ","
-    parsed += str(int.from_bytes(data[130:134])) + ","
-    parsed += str(int.from_bytes(data[140:142])) + ","
-    parsed += str(int.from_bytes(data[148:150])) + ","
-    parsed += str(int.from_bytes(data[156:158])) + ","
-    parsed += str(int.from_bytes(data[167:170])) + ","
-    parsed += str(int.from_bytes(data[176:178])) + ","
-    parsed += str(int.from_bytes(data[186:190])) + ","
-    parsed += str(int.from_bytes(data[196:198])) + ","
-    parsed += str(int.from_bytes(data[207:210])) + ","
-    parsed += str(int.from_bytes(data[216:218])) + ","
-    parsed += str(int.from_bytes(data[227:230])) + ","
-    parsed += str(int.from_bytes(data[236:238])) + ","
-    parsed += str(int.from_bytes(data[246:250])) + ","
-    parsed += str(int.from_bytes(data[256:258])) + ","
-    parsed += str(int.from_bytes(data[266:270])) + ","
-    parsed += str(int.from_bytes(data[276:278])) + ","
-    parsed += str(int.from_bytes(data[284:286])) + ","
-    parsed += str(int.from_bytes(data[292:294])) + ","
-    parsed += str(int.from_bytes(data[300:302])) + ","
-    parsed += str(int.from_bytes(data[310:314])) + ","
-    parsed += str(int.from_bytes(data[320:322])) + ","
-    parsed += str(int.from_bytes(data[330:334])) + ","
-    parsed += str(int.from_bytes(data[340:342])) + ","
-    parsed += str(int.from_bytes(data[350:354])) + ","
-    parsed += str(int.from_bytes(data[360:362])) + ","
-    parsed += str(int.from_bytes(data[370:374])) + ","
-    parsed += str(int.from_bytes(data[380:382])) + ","
-    parsed += str(int.from_bytes(data[390:394])) + ","
-    parsed += str(int.from_bytes(data[400:402])) + ","
-    parsed += str(int.from_bytes(data[410:414])) + ","
-    parsed += str(int.from_bytes(data[420:422])) + ","
-    parsed += str(int.from_bytes(data[428:430])) + ","
-    parsed += str(int.from_bytes(data[436:438])) + ","
-    parsed += str(int.from_bytes(data[444:446])) + ","
-    parsed += str(int.from_bytes(data[455:458])) + ","
-    parsed += str(int.from_bytes(data[464:466])) + ","
-    parsed += str(int.from_bytes(data[474:478])) + ","
-    parsed += str(int.from_bytes(data[484:486])) + ","
-    parsed += str(int.from_bytes(data[495:498])) + ","
-    parsed += str(int.from_bytes(data[504:506])) + ","
-    parsed += str(int.from_bytes(data[514:518])) + ","
-    parsed += str(int.from_bytes(data[524:526])) + ","
-    parsed += str(int.from_bytes(data[534:538])) + ","
-    parsed += str(int.from_bytes(data[544:546])) + ","
-    parsed += str(int.from_bytes(data[554:558])) + ","
-    parsed += str(int.from_bytes(data[564:566])) + ","
-    parsed += str(int.from_bytes(data[572:574])) + ","
-    parsed += str(int.from_bytes(data[580:582])) + ","
+    parsed = dict()
+    parsed["uptime_millis"] = int.from_bytes(data[10:14])
+    
+    parsed["column03"] = int.from_bytes(data[20:22])
+    parsed["cumulative_column03"] = int.from_bytes(data[31:34])
+
+    parsed["export_tenths_watt"] = int.from_bytes(data[39:42])
+    parsed["cumulative_export"] = int.from_bytes(data[50:54])
+
+    parsed["column07"] = int.from_bytes(data[60:62])
+    parsed["cumulative_column07"] = int.from_bytes(data[70:74])
+
+    parsed["column09"] = int.from_bytes(data[80:82])
+    parsed["cumulative_column09"] = int.from_bytes(data[90:94])
+
+    parsed["column11"] = int.from_bytes(data[100:102])
+    parsed["cumulative_column11"] = int.from_bytes(data[111:114])
+
+    parsed["column13"] = int.from_bytes(data[119:122])
+    parsed["cumulative_column13"] = int.from_bytes(data[130:134])
+
+    parsed["column15"] = int.from_bytes(data[140:142])
+    parsed["column16"] = int.from_bytes(data[148:150])
+
+    parsed["column17"] = int.from_bytes(data[156:158])
+    parsed["cumulative_column17"] = int.from_bytes(data[167:170])
+
+    parsed["column19"] = int.from_bytes(data[176:178])
+    parsed["cumulative_column19"] = int.from_bytes(data[186:190])
+
+    parsed["column21"] = int.from_bytes(data[196:198])
+    parsed["cumulative_column21"] = int.from_bytes(data[207:210])
+
+    parsed["column23"] = int.from_bytes(data[216:218])
+    parsed["column24"] = int.from_bytes(data[227:230])
+
+    parsed["column25"] = int.from_bytes(data[236:238])
+    parsed["cumulative_column25"] = int.from_bytes(data[246:250])
+
+    parsed["column27"] = int.from_bytes(data[256:258])
+    parsed["cumulative_column27"] = int.from_bytes(data[266:270])
+
+    parsed["column29"] = int.from_bytes(data[276:278])
+    parsed["column30"] = int.from_bytes(data[284:286])
+    parsed["column31"] = int.from_bytes(data[292:294])
+    parsed["column32"] = int.from_bytes(data[300:302])
+    parsed["column33"] = int.from_bytes(data[310:314])
+
+    parsed["column34"] = int.from_bytes(data[320:322])
+    parsed["cumulative_column34"] = int.from_bytes(data[330:334])
+
+    parsed["column36"] = int.from_bytes(data[340:342])
+    parsed["cumulative_column36"] = int.from_bytes(data[350:354])
+
+    parsed["column38"] = int.from_bytes(data[360:362])
+    parsed["cumulative_column38"] = int.from_bytes(data[370:374])
+
+    parsed["column40"] = int.from_bytes(data[380:382])
+    parsed["column41"] = int.from_bytes(data[390:394])
+
+    parsed["column42"] = int.from_bytes(data[400:402])
+    parsed["cumulative_column42"] = int.from_bytes(data[410:414])
+
+    parsed["column44"] = int.from_bytes(data[420:422])
+    parsed["column45"] = int.from_bytes(data[428:430])
+    parsed["column46"] = int.from_bytes(data[436:438])
+
+    parsed["column47"] = int.from_bytes(data[444:446])
+    parsed["cumulative_column47"] = int.from_bytes(data[455:458])
+
+    parsed["column49"] = int.from_bytes(data[464:466])
+    parsed["cumulative_column49"] = int.from_bytes(data[474:478])
+
+    parsed["column51"] = int.from_bytes(data[484:486])
+    parsed["cumulative_column51"] = int.from_bytes(data[495:498])
+
+    parsed["column53"] = int.from_bytes(data[504:506])
+    parsed["cumulative_column53"] = int.from_bytes(data[514:518])
+
+    parsed["column55"] = int.from_bytes(data[524:526])
+    parsed["cumulative_column55"] = int.from_bytes(data[534:538])
+
+    parsed["column57"] = int.from_bytes(data[544:546])
+    parsed["cumulative_column57"] = int.from_bytes(data[554:558])
+
+    parsed["column59"] = int.from_bytes(data[564:566])
+    parsed["column60"] = int.from_bytes(data[572:574])
+    parsed["column61"] = int.from_bytes(data[580:582])
+
     return parsed
 
 
@@ -168,13 +198,23 @@ def parse_file(path):
 
                 # find udp packets
                 if isinstance(eth.data, dpkt.ip.IP) and isinstance(eth.data.data, dpkt.udp.UDP):
-                    raw, parsed = parse_sma_packet(eth.data)
-                    if parsed:
+                    try:
+                        raw, parsed = parse_sma_packet(eth.data)
+
+                        # keep the raw packet data so we can find changing and fixed bytes
                         all_packets.append(raw)
+
+                        # print each packet so we can inspect visually
                         print(f"{datetime.utcfromtimestamp(ts)}: {parsed}")
+
+                        # Export the first datapoint each minute to "out.csv"
                         if ts%60 < last_ts%60:
                             csv.write(f"{datetime.utcfromtimestamp(ts)},{parsed}\n")
+
                         last_ts = ts
+                        
+                    except UnkownProtocolException:
+                        pass
 
     return all_packets
 
