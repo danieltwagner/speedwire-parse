45b87848082d1d974ee0c0c072f37c959a36e548
Daniel Wagner <dtw@dropbox.com>
2023-06-04 19:53:11 +0100

    charging data

--- parse.py
+++ parse.py
@@ -52,43 +52,66 @@ def parse_sma_net_packet(data):
 
     parsed = ""
     # The changing bytes
-    parsed += data[11:14].hex() + " "
-    parsed += data[40:42].hex() + " "
-    parsed += data[51:54].hex() + " "
-    parsed += data[60:62].hex() + " "
-    parsed += data[72:74].hex() + " "
-    parsed += data[80:82].hex() + " "
-    parsed += data[92:94].hex() + " "
-    parsed += data[120:122].hex() + " "
-    parsed += data[131:134].hex() + " "
-    parsed += data[148:150].hex() + " "
-    parsed += data[176:178].hex() + " "
-    parsed += data[187:190].hex() + " "
-    parsed += data[196:198].hex() + " "
-    parsed += data[207:210].hex() + " "
-    parsed += data[256:258].hex() + " "
-    parsed += data[267:270].hex() + " "
-    parsed += data[276:278].hex() + " "
-    parsed += data[284:286].hex() + " "
-    parsed += data[293:294].hex() + " "
-    parsed += data[320:322].hex() + " "
-    parsed += data[331:334].hex() + " "
-    parsed += data[341:342].hex() + " "
-    parsed += data[360:362].hex() + " "
-    parsed += data[372:374].hex() + " "
-    parsed += data[400:402].hex() + " "
-    parsed += data[411:414].hex() + " "
-    parsed += data[420:422].hex() + " "
-    parsed += data[428:430].hex() + " "
-    parsed += data[437:438].hex() + " "
-    parsed += data[464:466].hex() + " "
-    parsed += data[475:478].hex() + " "
-    parsed += data[504:506].hex() + " "
-    parsed += data[515:518].hex() + " "
-    parsed += data[544:546].hex() + " "
-    parsed += data[555:558].hex() + " "
-    parsed += data[564:566].hex() + " "
-    parsed += data[572:574].hex() + " "
+    parsed += str(int.from_bytes(data[10:14])) + ","
+    parsed += str(int.from_bytes(data[20:22])) + ","
+    parsed += str(int.from_bytes(data[31:34])) + ","
+    parsed += str(int.from_bytes(data[39:42])) + ","
+    parsed += str(int.from_bytes(data[50:54])) + ","
+    parsed += str(int.from_bytes(data[60:62])) + ","
+    parsed += str(int.from_bytes(data[70:74])) + ","
+    parsed += str(int.from_bytes(data[80:82])) + ","
+    parsed += str(int.from_bytes(data[90:94])) + ","
+    parsed += str(int.from_bytes(data[100:102])) + ","
+    parsed += str(int.from_bytes(data[111:114])) + ","
+    parsed += str(int.from_bytes(data[119:122])) + ","
+    parsed += str(int.from_bytes(data[130:134])) + ","
+    parsed += str(int.from_bytes(data[140:142])) + ","
+    parsed += str(int.from_bytes(data[148:150])) + ","
+    parsed += str(int.from_bytes(data[156:158])) + ","
+    parsed += str(int.from_bytes(data[167:170])) + ","
+    parsed += str(int.from_bytes(data[176:178])) + ","
+    parsed += str(int.from_bytes(data[186:190])) + ","
+    parsed += str(int.from_bytes(data[196:198])) + ","
+    parsed += str(int.from_bytes(data[207:210])) + ","
+    parsed += str(int.from_bytes(data[216:218])) + ","
+    parsed += str(int.from_bytes(data[227:230])) + ","
+    parsed += str(int.from_bytes(data[236:238])) + ","
+    parsed += str(int.from_bytes(data[246:250])) + ","
+    parsed += str(int.from_bytes(data[256:258])) + ","
+    parsed += str(int.from_bytes(data[266:270])) + ","
+    parsed += str(int.from_bytes(data[276:278])) + ","
+    parsed += str(int.from_bytes(data[284:286])) + ","
+    parsed += str(int.from_bytes(data[292:294])) + ","
+    parsed += str(int.from_bytes(data[300:302])) + ","
+    parsed += str(int.from_bytes(data[310:314])) + ","
+    parsed += str(int.from_bytes(data[320:322])) + ","
+    parsed += str(int.from_bytes(data[330:334])) + ","
+    parsed += str(int.from_bytes(data[340:342])) + ","
+    parsed += str(int.from_bytes(data[350:354])) + ","
+    parsed += str(int.from_bytes(data[360:362])) + ","
+    parsed += str(int.from_bytes(data[370:374])) + ","
+    parsed += str(int.from_bytes(data[380:382])) + ","
+    parsed += str(int.from_bytes(data[390:394])) + ","
+    parsed += str(int.from_bytes(data[400:402])) + ","
+    parsed += str(int.from_bytes(data[410:414])) + ","
+    parsed += str(int.from_bytes(data[420:422])) + ","
+    parsed += str(int.from_bytes(data[428:430])) + ","
+    parsed += str(int.from_bytes(data[436:438])) + ","
+    parsed += str(int.from_bytes(data[444:446])) + ","
+    parsed += str(int.from_bytes(data[455:458])) + ","
+    parsed += str(int.from_bytes(data[464:466])) + ","
+    parsed += str(int.from_bytes(data[474:478])) + ","
+    parsed += str(int.from_bytes(data[484:486])) + ","
+    parsed += str(int.from_bytes(data[495:498])) + ","
+    parsed += str(int.from_bytes(data[504:506])) + ","
+    parsed += str(int.from_bytes(data[514:518])) + ","
+    parsed += str(int.from_bytes(data[524:526])) + ","
+    parsed += str(int.from_bytes(data[534:538])) + ","
+    parsed += str(int.from_bytes(data[544:546])) + ","
+    parsed += str(int.from_bytes(data[554:558])) + ","
+    parsed += str(int.from_bytes(data[564:566])) + ","
+    parsed += str(int.from_bytes(data[572:574])) + ","
+    parsed += str(int.from_bytes(data[580:582])) + ","
     return parsed
 
 
@@ -113,7 +136,7 @@ def find_constant_elements(all_packets):
     print(f"Print/parse the {len(constant_byte_mask)} changing bytes like so:")
     changing_bytes = [x for x in range(packet_length) if x not in constant_byte_mask]
     for start, end in group_ranges(changing_bytes):
-            print(f'parsed += data[{start}:{end+1}].hex() + " "')
+            print(f'parsed += str(int.from_bytes([{start}:{end+1}])) + ","')
 
 
 
@@ -133,22 +156,36 @@ def group_ranges(sorted_list):
     return ranges
 
 
-def main(path):
-    print(f"Parsing {path}...")
+def parse_file(path):
+    all_packets = []
+    last_ts = 0
+
+    with open(path, 'rb') as f:
+        with open('out.csv', 'w') as csv:
+            pcap = dpkt.pcap.Reader(f)
+            for ts, buf in pcap:
+                eth = dpkt.ethernet.Ethernet(buf)
+
+                # find udp packets
+                if isinstance(eth.data, dpkt.ip.IP) and isinstance(eth.data.data, dpkt.udp.UDP):
+                    raw, parsed = parse_sma_packet(eth.data)
+                    if parsed:
+                        all_packets.append(raw)
+                        print(f"{datetime.utcfromtimestamp(ts)}: {parsed}")
+                        if ts%60 < last_ts%60:
+                            csv.write(f"{datetime.utcfromtimestamp(ts)},{parsed}\n")
+                        last_ts = ts
+
+    return all_packets
+
+
+def main(paths):
     
     all_packets = []
 
-    with open(path, 'rb') as f:
-        pcap = dpkt.pcap.Reader(f)
-        for ts, buf in pcap:
-            eth = dpkt.ethernet.Ethernet(buf)
-
-            # find udp packets
-            if isinstance(eth.data, dpkt.ip.IP) and isinstance(eth.data.data, dpkt.udp.UDP):
-                raw, parsed = parse_sma_packet(eth.data)
-                if parsed:
-                    all_packets.append(raw)
-                    print(f"{datetime.utcfromtimestamp(ts)}: {parsed}")
+    for path in paths:
+        print(f"Parsing {path}...")
+        all_packets += parse_file(path)
 
     print(f"Found {len(all_packets)} packets")
     find_constant_elements(all_packets)
@@ -156,7 +193,7 @@ def main(path):
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print(f"Usage: {sys.argv[0]} pcap_file")
+        print(f"Usage: {sys.argv[0]} pcap_file [pcap_file2, ...]")
         sys.exit(1)
     
-    main(sys.argv[1])
+    main(sys.argv[1:])
